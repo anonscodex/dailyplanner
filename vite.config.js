1baaaed7f2a140d5c66db0b38134ffd4e824190f
anonscodex <normalstuff2001@gmail.com>
2025-01-16 21:09:31 +0100

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    global: "window", // Ensures global references work
+  },
+  resolve: {
+    alias: {
+      buffer: "buffer",
+    },
+  },
+  build: {
+    target: "esnext", // Prevent polyfills from being stripped
+    rollupOptions: {
+      output: {
+        manualChunks: undefined, // Avoid splitting too aggressively
+      },
+    },
+  },
+  
 })
